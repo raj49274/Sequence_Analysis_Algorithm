82333e012faf06afe64edcf396f0288e0ec1b4bb
Aditya Raj <raj49274@gmail.com>
2022-06-13 10:59:00 +0530

    created graph

--- project.py
+++ project.py
@@ -1,11 +1,20 @@
 # Project : Sequence Analysis Algorithm
 # 
+# Authors: Aditya Raj (AU1920177)
+#        : Sanya Zaveri (AU1920064)
 # 
 
 
 
 #------------------- Imports ----------------
 import argparse
+
+
+#------------------global variables -------------
+prefixes = {}
+suffixes = []
+no_of_reads = 0
+read_size = 0
 
 
 # ---------- Helper Functions ------------------------
@@ -48,6 +57,14 @@ def calculateValue(x):
         i += 1
     return res
 
+# getting read size
+def getReadSize(file_name):
+    global read_size
+    with open(file_name, "r") as file: 
+        data = file.readline()
+        read_size = len(data)
+            
+
 
 # ------------ Function to create prefix and storing in a data structure --------
 
@@ -70,7 +87,7 @@ def createPrefix(read, readId, minLength, prefix_index):
     prefix_index[(fingerprint, minLength)] = [readId]
     
     # starting consuming characters
-    for i in range(minLength, len(read), 1):
+    for i in range(minLength, len(read) - 1, 1):
         fingerprint = consume_char_at_right(fingerprint, read[i])
         # checking and adding the fingerprint
         if (fingerprint, i+1) in prefix_index:
@@ -88,14 +105,14 @@ def createPrefix(read, readId, minLength, prefix_index):
 # Assuming the function takes f, n is suffix length and c as character
 def consume_char_from_left(f, n, c):
     c_int = encode(c)
-    return f + (c_int*(5**n))%prime
+    return (f + c_int*(5**(n-1)))%prime
 
 # Crating suffix from minLength of suffix
 def createSuffix(read, readId, minLength, suffix_index):
     # calculating starting fingerprint
     fingerprint = 0
     readLength = len(read)
-    startIndex = readLength - minLength - 1
+    startIndex = readLength - minLength
     suffixLength = minLength
     
     for i in range(startIndex, readLength, 1):
@@ -118,13 +135,25 @@ def createSuffix(read, readId, minLength, suffix_index):
 
 #-----------Creating Fingerprint-----------------
 def createFingerprint(file_name, minLength):
+    global no_of_reads
     prefix_index = {}
     suffix_index = []
     # opening the file and reading the line by line
     with open(file_name, "r") as file: 
         data = file.readlines()
         i = 0
+        j = 0
         for line in data:
+            if j == 0:
+                j = 1
+                continue
+            else:
+                j = 0
+                
+            no_of_reads = no_of_reads + 1
+            if no_of_reads == 20000:
+                break
+            
             word = line.split()[0]
             
             # Now calculating prefix and suffix
@@ -144,35 +173,50 @@ def createFingerprint(file_name, minLength):
 
 # ----------- Creating graph ------------------
 
-def createGraph(r):
+def createGraph(pfix, sfix, r):
     graph = []
     
     # Assigning None to every element in the graph
     for i in range(r):
-        graph[i] = None
+        graph.append(None)
+        
+    sfix.sort(reverse= True)
     
-    
+    for i in sfix:
+        if graph[i[2]] != None and graph[i[2]][0][1] > i[0] and i[0] < 85:
+            continue
+        else:
+            lp = pfix.get((i[1], i[0]))
+            if lp != None:
+                if graph[i[2]] == None:
+                    graph[i[2]] = []
+                for j in lp:
+                    graph[i[2]].append((j, i[0]))
+                    
+    with open("graph.gfa", "w") as file:
+        for i in range(r):
+            if graph[i] != None:
+                file.write(f"S\t{i+1}\t*\tLN:i:{read_size}\n")
+                for k in graph[i]:
+                    file.write(f"L\t{i+1}\t+\t{k[0]}\t+\t{k[1]}M\n")
+        
+        
+    print(graph)
     return 0
 
 
 # -------- Driver Function -----------
 def main():
-    
-    prefixes = {}
-    suffixes = [] 
+    global prefixes, suffixes
     minLength = args.minLength
     file_name = args.fileName
     
+    getReadSize(file_name)
+    
     prefixes, suffixes  = createFingerprint(file_name, minLength)
     
-    
-    print(prefixes)
-    print(suffixes)
+    createGraph(prefixes, suffixes, no_of_reads*2)
     return 0
-
-
-
-
 
 
 # -------------- Running the file ----------
@@ -180,10 +224,12 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="taking inputs: prime, text, pattern")
     
     # Adding Arguments and parsing it
-    parser.add_argument('-f', '--fileName', default="input.txt", type=str)
+    parser.add_argument('-f', '--fileName', default="reads.fasta", type=str)
     parser.add_argument('-p', '--prime', default=1073741789, type=int)
-    parser.add_argument('-l', '--minLength', default= 5, type=int)
+    parser.add_argument('-l', '--minLength', default=5, type=int)
     args = parser.parse_args()
     
     prime =  args.prime
+    
+
     main()
